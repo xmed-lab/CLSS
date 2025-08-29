4b1a28efab73048cd789fa3dba075c7af08c3046
cewang <fogever@163.com>
2025-08-29 15:35:26 +0800

    Add files via upload

--- UTKFace/GCLSS/utils/__init__.py
+++ UTKFace/GCLSS/utils/__init__.py
@@ -1,1 +1,101 @@
 
+import os
+import typing
+import datetime
+
+import cv2  
+import matplotlib
+import numpy as np
+import torch
+import tqdm
+
+
+def get_mean_and_std(dataset: torch.utils.data.Dataset,
+                     samples: int = 128,
+                     batch_size: int = 8,
+                     num_workers: int = 4):
+    
+
+    if samples is not None and len(dataset) > samples:
+        indices = np.random.choice(len(dataset), samples, replace=False)
+        dataset = torch.utils.data.Subset(dataset, indices)
+    
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, num_workers=num_workers, shuffle=True)
+
+    n = 0  
+    s1 = 0.  
+    s2 = 0.  
+
+    for (x,_,*_) in tqdm.tqdm(dataloader):
+        x = x.transpose(0, 1).contiguous().view(3, -1)
+        n += x.shape[1]
+        s1 += torch.sum(x, dim=1).numpy()
+        s2 += torch.sum(x ** 2, dim=1).numpy()
+    mean = s1 / n  
+    std = np.sqrt(s2 / n - mean ** 2) 
+
+    mean = mean.astype(np.float32)
+    std = std.astype(np.float32)
+
+    return mean, std
+
+
+def bootstrap(a, b, func, samples=10000):
+    """Computes a bootstrapped confidence intervals for ``func(a, b)''.
+
+    Args:
+        a (array_like): first argument to `func`.
+        b (array_like): second argument to `func`.
+        func (callable): Function to compute confidence intervals for.
+            ``dataset[i][0]'' is expected to be the i-th video in the dataset, which
+            should be a ``torch.Tensor'' of dimensions (channels=3, frames, height, width)
+        samples (int, optional): Number of samples to compute.
+            Defaults to 10000.
+
+    Returns:
+       A tuple of (`func(a, b)`, estimated 5-th percentile, estimated 95-th percentile).
+    """
+    a = np.array(a)
+    b = np.array(b)
+
+    bootstraps = []
+    for _ in range(samples):
+        ind = np.random.choice(len(a), len(a))
+        bootstraps.append(func(a[ind], b[ind]))
+    bootstraps = sorted(bootstraps)
+
+    return func(a, b), bootstraps[round(0.05 * len(bootstraps))], bootstraps[round(0.95 * len(bootstraps))]
+
+
+def latexify():
+    """Sets matplotlib params to appear more like LaTeX.
+
+    Based on https://nipunbatra.github.io/blog/2014/latexify.html
+    """
+    params = {'backend': 'pdf',
+              'axes.titlesize': 8,
+              'axes.labelsize': 8,
+              'font.size': 8,
+              'legend.fontsize': 8,
+              'xtick.labelsize': 8,
+              'ytick.labelsize': 8,
+              'font.family': 'DejaVu Serif',
+              'font.serif': 'Computer Modern',
+              }
+    matplotlib.rcParams.update(params)
+
+
+def dice_similarity_coefficient(inter, union):
+    """Computes the dice similarity coefficient.
+
+    Args:
+        inter (iterable): iterable of the intersections
+        union (iterable): iterable of the unions
+    """
+    return 2 * sum(inter) / (sum(union) + sum(inter))
+
+
+# __all__ = ["get_mean_and_std"]
+__all__ = ["video", "segmentation", "seg_ctrmlt", "seg_sslflw", "vidseg", "video_segin", "videossl", "loadvideo", "savevideo", "get_mean_and_std", "bootstrap", "latexify", "dice_similarity_coefficient"]
+
