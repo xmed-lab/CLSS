004a80bafb1ed9a36c649f616e7ea22c97916ad6
cewang <fogever@163.com>
2025-08-29 12:33:34 +0800

    Add files via upload

---
+++ AgeDB-DIR/CLSS/README.md
@@ -0,0 +1,21 @@
+
+## Experiments on the AgeDB-DIR dataset
+
+This is the code for running the experiments for the age estimation task
+
+### Data
+
+The dataset can be downloaded from: https://ibug.doc.ic.ac.uk/resources/agedb/
+
+
+## Running
+
+Run scripts for CPS and CLSS are provided in script_cps.sh and scripts_clss.sh respectively
+
+
+## Pretrained models
+
+A sample checkpoint using 1/20 labeled data is provided: 
+https://hkustconnect-my.sharepoint.com/:f:/g/personal/wdaiaj_connect_ust_hk/ErCKRXmHFmRLq2XoJiufrhkBiBhIEp11iDpWKa1odtDviA?e=lHYFh7
+
+
---
+++ AgeDB-DIR/CLSS/scripts_cps.sh
@@ -0,0 +1,77 @@
+
+
+# 1/30
+
+export CUDA_VISIBLE_DEVICES=4
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s1 --num_epochs=30 --reduced_set --rd_label=407 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=6 --val_samp=1 --lr=0.0005 --seed=1 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s1.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s2 --num_epochs=30 --reduced_set --rd_label=407 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=6 --val_samp=1 --lr=0.0005 --seed=2 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s2.log 2>&1 ;} &
+export CUDA_VISIBLE_DEVICES=5
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s3 --num_epochs=30 --reduced_set --rd_label=407 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=6 --val_samp=1 --lr=0.0005 --seed=3 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s3.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s4 --num_epochs=30 --reduced_set --rd_label=407 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=6 --val_samp=1 --lr=0.0005 --seed=4 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s4.log 2>&1 ;} &
+export CUDA_VISIBLE_DEVICES=6
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s5 --num_epochs=30 --reduced_set --rd_label=407 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=6 --val_samp=1 --lr=0.0005 --seed=5 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s5.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s6 --num_epochs=30 --reduced_set --rd_label=407 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=6 --val_samp=1 --lr=0.0005 --seed=6 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s6.log 2>&1 ;} &
+export CUDA_VISIBLE_DEVICES=7
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s7 --num_epochs=30 --reduced_set --rd_label=407 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=6 --val_samp=1 --lr=0.0005 --seed=7 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s7.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s8 --num_epochs=30 --reduced_set --rd_label=407 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=6 --val_samp=1 --lr=0.0005 --seed=8 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov30_s8.log 2>&1 ;} &
+
+
+
+
+
+
+# 1/25
+
+
+export CUDA_VISIBLE_DEVICES=4
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s1 --num_epochs=30 --reduced_set --rd_label=488 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=5 --val_samp=1 --lr=0.0005 --seed=1 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s1.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s2 --num_epochs=30 --reduced_set --rd_label=488 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=5 --val_samp=1 --lr=0.0005 --seed=2 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s2.log 2>&1 ;} &
+export CUDA_VISIBLE_DEVICES=5
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s3 --num_epochs=30 --reduced_set --rd_label=488 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=5 --val_samp=1 --lr=0.0005 --seed=3 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s3.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s4 --num_epochs=30 --reduced_set --rd_label=488 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=5 --val_samp=1 --lr=0.0005 --seed=4 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s4.log 2>&1 ;} &
+export CUDA_VISIBLE_DEVICES=6
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s5 --num_epochs=30 --reduced_set --rd_label=488 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=5 --val_samp=1 --lr=0.0005 --seed=5 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s5.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s6 --num_epochs=30 --reduced_set --rd_label=488 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=5 --val_samp=1 --lr=0.0005 --seed=6 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s6.log 2>&1 ;} &
+export CUDA_VISIBLE_DEVICES=7
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s7 --num_epochs=30 --reduced_set --rd_label=488 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=5 --val_samp=1 --lr=0.0005 --seed=7 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s7.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s8 --num_epochs=30 --reduced_set --rd_label=488 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=5 --val_samp=1 --lr=0.0005 --seed=8 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov25_s8.log 2>&1 ;} &
+
+
+
+
+
+# 1/20
+
+
+
+export CUDA_VISIBLE_DEVICES=4
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s1 --num_epochs=30 --reduced_set --rd_label=610 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=4 --val_samp=1 --lr=0.0005 --seed=1 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s1.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s2 --num_epochs=30 --reduced_set --rd_label=610 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=4 --val_samp=1 --lr=0.0005 --seed=2 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s2.log 2>&1 ;} &
+export CUDA_VISIBLE_DEVICES=5
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s3 --num_epochs=30 --reduced_set --rd_label=610 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=4 --val_samp=1 --lr=0.0005 --seed=3 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s3.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s4 --num_epochs=30 --reduced_set --rd_label=610 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=4 --val_samp=1 --lr=0.0005 --seed=4 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s4.log 2>&1 ;} &
+export CUDA_VISIBLE_DEVICES=6
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s5 --num_epochs=30 --reduced_set --rd_label=610 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=4 --val_samp=1 --lr=0.0005 --seed=5 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s5.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s6 --num_epochs=30 --reduced_set --rd_label=610 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=4 --val_samp=1 --lr=0.0005 --seed=6 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s6.log 2>&1 ;} &
+export CUDA_VISIBLE_DEVICES=7
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s7 --num_epochs=30 --reduced_set --rd_label=610 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=4 --val_samp=1 --lr=0.0005 --seed=7 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s7.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s8 --num_epochs=30 --reduced_set --rd_label=610 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=4 --val_samp=1 --lr=0.0005 --seed=8 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov20_s8.log 2>&1 ;} &
+
+
+
+
+# 1/15
+
+
+export CUDA_VISIBLE_DEVICES=4
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s1 --num_epochs=30 --reduced_set --rd_label=814 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=3 --val_samp=1 --lr=0.0005 --seed=1 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s1.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s2 --num_epochs=30 --reduced_set --rd_label=814 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=3 --val_samp=1 --lr=0.0005 --seed=2 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s2.log 2>&1 ;} &
+export CUDA_VISIBLE_DEVICES=5
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s3 --num_epochs=30 --reduced_set --rd_label=814 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=3 --val_samp=1 --lr=0.0005 --seed=3 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s3.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s4 --num_epochs=30 --reduced_set --rd_label=814 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=3 --val_samp=1 --lr=0.0005 --seed=4 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s4.log 2>&1 ;} &
+export CUDA_VISIBLE_DEVICES=6
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s5 --num_epochs=30 --reduced_set --rd_label=814 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=3 --val_samp=1 --lr=0.0005 --seed=5 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s5.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s6 --num_epochs=30 --reduced_set --rd_label=814 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=3 --val_samp=1 --lr=0.0005 --seed=6 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s6.log 2>&1 ;} &
+export CUDA_VISIBLE_DEVICES=7
+{ nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s7 --num_epochs=30 --reduced_set --rd_label=814 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=3 --val_samp=1 --lr=0.0005 --seed=7 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s7.log 2>&1 ; \
+nohup python3 vol_reg_oedNP_semi_ulbsep_ranksim_cps_b2.py --run_test --quick_test --batch_size=32 --output=output/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s8 --num_epochs=30 --reduced_set --rd_label=814 --rd_unlabel=0 --allsplt --w_cps=1 --ssl_mult=3 --val_samp=1 --lr=0.0005 --seed=8 --w_ctr=1 --w_ctrulb_0=0.05 --w_ctrulb_1=0 --lambda_val=2 --batch_size_two=8  > logs/220513_odeNPCPS2B8_wcps1_wctr1_wct0u5e2_wct1u0_lmb2_1ov15_s8.log 2>&1 ;} &
